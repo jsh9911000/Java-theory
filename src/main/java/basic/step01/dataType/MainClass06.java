aefb3cdc0d3b1412d97f3f80c4bc2aa0dbccbee8
jsh <jsh9911000@naver.com>
2026-07-14 22:28:01 +0900

    Access Modifier complete

---
+++ src/main/java/basic/step01/dataType/MainClass06.java
@@ -0,0 +1,16 @@
+package basic.step01.dataType;
+
+public class MainClass06 {
+
+	public static void main(String[] args) {
+		/*
+		 *	접근제어자
+		 *	- public: 어디서든 사용할 수 있다.
+		 *	- protected: 같은 클래스, 같은 패키지, 상속받은 다른 패키지에서만 사용할 수 있다.
+		 *	- default: 같은 클래스, 같은 패키지에서만 사용할 수 있다.
+		 *	- private: 같은 클래스 내에서만 사용할 수 있다.
+		 */
+
+	}
+
+}
